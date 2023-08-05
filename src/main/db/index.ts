d7b31f53b4ede87482c371ed331b14c8e3a226bc
Michael <mckchan13@gmail.com>
2023-08-05 10:00:59 -0700

    refactored database file to singleton pattern to prevent multiple db instances from initalizing

--- src/main/db/index.ts
+++ src/main/db/index.ts
@@ -1,31 +1,51 @@
 import * as sqlite3 from "sqlite3";
 
-export async function startDb() {
-  const db = new sqlite3.Database(":memory:");
+export class SqlDatabase {
+  static #instance: SqlDatabase;
+  private db: sqlite3.Database;
+  private constructor() {
+    this.db = new sqlite3.Database(":memory:");
+  }
 
-  db.serialize(() => {
-    db.run(`CREATE TABLE passwords (
-    descriptor TEXT NOT NULL, 
-    password TEXT NOT NULL
-    )`);
-
-    console.log("Table created, now preparing insert statement");
-    const stmt = db.prepare(`INSERT INTO passwords VALUES ($desc, $pass);`);
-
-    for (let i = 0; i < 10; i++) {
-      stmt.run(i, i);
+  public static get instance(): SqlDatabase {
+    if (!this.#instance) {
+      return new SqlDatabase();
     }
 
-    console.log("finalizing statement");
-    stmt.finalize();
+    return this.#instance;
+  }
 
-    db.each(
-      "SELECT rowid AS id, descriptor, password FROM passwords",
-      (_, row: { id: string; descriptor: string; password: string }) => {
-        console.log(row);
+  public initDb(): void {
+    const db = this.db;
+
+    db.serialize(() => {
+      db.run(
+        `CREATE TABLE passwords (
+          descriptor TEXT NOT NULL, 
+          password TEXT NOT NULL
+          )`
+      );
+
+      console.log("Table created, now preparing insert statement");
+      const stmt = db.prepare(`INSERT INTO passwords VALUES ($desc, $pass);`);
+
+      for (let i = 0; i < 10; i++) {
+        stmt.run(i, i);
       }
-    );
-  });
 
-  db.close();
+      console.log("Finalizing statement");
+      stmt.finalize();
+
+      db.each(
+        "SELECT rowid AS id, descriptor, password FROM passwords",
+        (_, row: { id: string; descriptor: string; password: string }) => {
+          console.log(row);
+        }
+      );
+    });
+  }
+
+  public closeDb(): void {
+    this.db.close();
+  }
 }
