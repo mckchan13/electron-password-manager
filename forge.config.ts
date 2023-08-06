bff7508ac2b7b455aa4ac52a6eb555e7980e05fd
Michael <mckchan13@gmail.com>
2023-08-06 11:57:38 -0700

    deleted all webpack related dependencies, config files, plugins and rebuilt sqlite3 and other node modules

--- forge.config.ts
+++ forge.config.ts
@@ -4,11 +4,6 @@ import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
-import { VitePlugin } from "@electron-forge/plugin-vite";
-// import { WebpackPlugin } from '@electron-forge/plugin-webpack';
-
-// import { mainConfig } from './webpack.main.config';
-// import { rendererConfig } from './webpack.renderer.config';
 
 const config: ForgeConfig = {
   packagerConfig: {
